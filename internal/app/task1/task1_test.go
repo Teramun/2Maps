5acedc0aaa6d7b9c2f68370e0435bae4fff1d93a
Teramun <teramun35@gmail.com>
2021-07-26 22:50:30 +0300

    fix task1_test.go

--- internal/app/task1/task1_test.go
+++ internal/app/task1/task1_test.go
@@ -33,8 +33,11 @@ func TestCompare(t *testing.T) {
 		result := Compare(value.numbersOne, value.numbersTwo)
 		for keyResult, valueResult := range result {
 			if valueResult != value.expected[keyResult] {
-				t.Log("BAD TEST", value.testName)
+				t.Log("BAD TEST (different values)", value.testName)
 			}
+		}
+		if len(result) != len(value.expected) {
+			t.Log("BAD TEST (different maps len)", value.testName)
 		}
 	}
 }
